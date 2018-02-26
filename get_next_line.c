3971be93e3c24ab93f80873308eb8faf62aac8de
Valeriia <33296101+vzamyati@users.noreply.github.com>
2018-02-26 20:06:06 +0200

    smth that doesn't work

--- get_next_line.c
+++ get_next_line.c
@@ -13,31 +13,46 @@
 #include <printf.h>
 #include "get_next_line.h"
 
-static int          searching(const int fd, char **line, char *buff)
+static int      reading(const int fd, char **line, char *buff)
 {
-    static char     *tmp;
-    ssize_t         rd;
+	static char     *str;
+	char *tmp = NULL;
+	char *next_line = NULL;
+	char *new_line = NULL;
+	int rd;
 
-    tmp = ft_strnew(1);
-    if (tmp != NULL)
-        *line = ft_strdup(buff);
-    while (!(ft_strchr(buff, '\n')) && (rd = read(fd, buff, BUFF_SIZE) > 0))
-    {
-        tmp = ft_strjoin(tmp, buff);
-        ft_bzero(buff, ft_strlen(buff));
-    }
-    *line = ft_strsub(tmp, 0, ft_strlen(tmp)); // search '\n' ;
-    if (ft_strlen(tmp) == 0)
-        return (0);
-    return (1);
+	if (!str)
+		str = ft_strnew(0);
+	while ((rd = read(fd, buff, BUFF_SIZE) > 0))
+	{
+		tmp = ft_strjoin(str, buff);
+		free (str);
+		ft_strclr(buff);
+		str = ft_strdup(tmp);
+		free (tmp);
+		if (next_line = ft_strchr(str, '\n'))
+			break ;
+	}
+	if (next_line)
+	{
+		*line = ft_strsub(str, 0, next_line - str);
+		str = ft_strdup(next_line + 1);
+		return (1);
+	}
+	*line = ft_strdup(str);
+	if ()
+	return (0);
 }
 
-int                 get_next_line(const int fd, char **line)
+int     get_next_line(const int fd, char **line)
 {
-    char            *buff;
+	char *buff;
 
-    buff = ft_strnew(BUFF_SIZE);
-    if (!(line == NULL || fd < 0))
-        return (searching(fd, &*line, buff));
-    return (-1);
+	buff = ft_strnew(BUFF_SIZE);
+	if (fd < 0 || (read(fd, buff, 0)) < 0 || line == NULL)
+		return (-1);
+	else
+		return (reading(fd, line, buff));
 }
+
+//		 str = (ft_strchr(buff,'\n') + 1);
